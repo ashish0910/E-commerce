32b8fdab922abaa37f5b16c3302e28cf07fb6b4a
ashish0910 <ashish09101998@gmail.com>
2020-11-13 09:36:09 +0530

    DB Setup

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// Middleware
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// connect to db
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
