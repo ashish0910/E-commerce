7c0735d181fd94a89a582cfb91fccaef0f4703e7
ashish0910 <ashish09101998@gmail.com>
2020-11-13 13:40:35 +0530

    User accesstoken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const Users = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userController = {
   register: async (req, res) => {
@@ -22,12 +23,51 @@ const userController = {
         email: email,
         password: hashedPassword,
       });
+
+      //   Save in db
       await newUser.save();
-      return res.json({ msg: "Successfully registered" });
+
+      //   JWT token for auth
+      const accesstoken = createAccessToken({ id: newUser._id });
+      const refreshtoken = createRefreshToken({ id: newUser._id });
+
+      res.cookie("refreshtoken", refreshtoken, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
+      });
+
+      res.json({ accesstoken });
+
+      //   res.json({ msg: "Successfully registered" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
   },
+  refreshToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token)
+        return res.status(400).json({ msg: "Please Login or Register" });
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(400).json({ msg: "Please Login or Register" });
+        const accesstoken = createAccessToken({ id: user.id });
+
+        res.json({ accesstoken });
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+const createAccessToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
+};
+const createRefreshToken = (user) => {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
 };
 
 module.exports = userController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 
 router.post("/register", userController.register);
+router.get("/refresh_token", userController.refreshToken);
 
 module.exports = router;
