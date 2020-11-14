e76eadd6e131ca189f3bfe674b2d6e35ef9b5f2c
ashish0910 <ashish09101998@gmail.com>
2020-11-14 16:59:17 +0530

    Header

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use("/api", require("./routes/categoryRoutes"));
 app.use("/api", require("./routes/upload"));
 app.use("/api", require("./routes/productRoutes"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
--- frontend/src/components/components/headers/Header.js
+++ frontend/src/components/components/headers/Header.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Menu from "./icons/menu.svg";
+import Close from "./icons/close.svg";
+import Cart from "./icons/cart.svg";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
     <div>
-      <header></header>
+      <header>
+        <div className="menu">
+          <img src={Menu} alt="" width="30" />
+        </div>
+        <div className="logo">
+          <h1>
+            <Link to="/">E-Commerce</Link>
+          </h1>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/login">Login / Register</Link>
+          </li>
+          <li>
+            <img src={Close} alt="" width="30" className="menu" />
+          </li>
+        </ul>
+        <div className="cart-icon">
+          <span>0</span>
+          <Link to="/cart">
+            <img src={Cart} alt="" width="30" />
+          </Link>
+        </div>
+      </header>
     </div>
   );
 }
