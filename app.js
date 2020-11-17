d14054deae4a7733141e2629774cb18e41fe8481
ashish0910 <ashish09101998@gmail.com>
2020-11-17 15:56:47 +0530

    Deployment

--- app.js
+++ app.js
@@ -40,6 +40,13 @@ app.use("/api", require("./routes/upload"));
 app.use("/api", require("./routes/productRoutes"));
 app.use("/api", require("./routes/paymentRoutes"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
